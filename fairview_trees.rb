b25c95b5ee326bc055e4ec4afabbbfd2b9f9ac86
Dan Gopstein <dan@gopstein.com>
2022-04-24 15:07:09 -0400

    Add in convexity testing

--- fairview_trees.rb
+++ fairview_trees.rb
@@ -1,56 +1,71 @@
 #!/bin/sh ruby
 
 require 'csv'
+require 'geometry'
 
-image_size = [4000, 3000]
+#image_size = [4000, 3000]
 
 tree_locations = CSV.read("tree_locations.csv", headers: true, converters: :numeric)
 
 trees = tree_locations.map(&:to_h).to_a
 
 def pixels_to_feet(px)
-  px / 11
+  px / 11.0
 end
 
-def area(quad)
-  a,b,c,d = quad
+trees.each{|t| t.merge!({'x' => pixels_to_feet(t['px']), 'y' => pixels_to_feet(t['py'])})}
+  .each{|t| t.merge!({'pt' => Geometry::Point[t['x'], t['y']]})}
 
-  px_area =
-  (a['x']*b['y']-a['y']*b['x'] + b['x']*c['y']-b['y']*c['x'] +
-   c['x']*d['y']-c['y']*d['x'] + c['x']*d['y']-c['y']*d['x']) / 2
+#def area(trees)
+#  (trees.each_cons(2).map{|a,b| a['x']*b['y']-a['y']*b['x']}.sum/2).abs
+#end
 
-  pixels_to_feet(px_area).abs
+def area(polygon)
+  (polygon.edges.map{|e| a = e.first; b = e.last; a.x*b.y-a.y*b.x}.sum/2).abs
+end
+area([t1, t2, t3])
+area([t1, t2, t3])
+
+def convex?(poly)
+  poly.convex.vertices.size == poly.vertices.size
 end
 
-def dist(a, b)
-  px_dist = Math.sqrt((b['x']-a['x'])**2 + (b['y']-a['y'])**2)
+#def dist(a, b)
+#  Math.sqrt((b['x']-a['x'])**2 + (b['y']-a['y'])**2)
+#end
 
-  pixels_to_feet(px_dist)
-end
+#def perim(trees)
+#  trees.each_cons(2).map{|a, b| dist(b, a)}.sum
+#end
 
-def perim(quad)
-  a,b,c,d = quad
-  dist(b, a) + dist(c, b) + dist(d, c) + dist(a, d)
+def perim(polygon)
+  polygon.edges.map(&:length).sum
 end
 
 # compare pixel coordinates to hand-measured benchmarks
 t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17=trees
 
-dist(t15, t3) # 94 ft
-dist(t15, t7) # 95 ft
-dist(t15, t10) # 88 ft
-dist(t15, t9) # 33 ft?
+#dist(t15, t3) # 94 ft
+#dist(t15, t7) # 95 ft
+#dist(t15, t10) # 88 ft
+#dist(t15, t9) # 33 ft?
 
 house_points = [t15, t16, t17]
 
-combos = trees.combination(4)
-  .map{|q| {trees: q.map(&:first).map(&:last), perim: perim(q), area: area(q)} }; nil
+combos = (trees.combination(3) + trees.combination(4) + trees.combination(5)); combos.size
 
-good_combos = combos
-     .filter{|combo| !(combo[:trees] & house_points.map{|t| t['name']}).empty?}
-     .filter{|combo| combo[:perim] > 420 && combo[:perim] < 440}
-     .sort_by{|combo| - combo[:area]}; good_combos.size
+polygons = combos.map{|c| {trees: c.map(&:first).map(&:last), polygon: Geometry::Polygon.new(*c.map{|t| t['pt']})}}
+                     .map{|c| c.merge({perim: perim(c[:polygon]).to_i, area: area(c[:polygon]).to_i}) }; nil
+
+antenna_length = 288
+ideal_perim = antenna_length + 80
+
+good_combos = polygons
+    .filter{|poly| convex?(poly[:polygon])}
+    .filter{|combo| !(combo[:trees] & house_points.map{|t| t['name']}).empty?}
+    .filter{|combo| (combo[:perim] - ideal_perim).abs < 30}
+    .sort_by{|combo| - combo[:area]}; good_combos.size
 
 good_combos.last
 
-puts; good_combos.take(50).map{|combo| puts combo}
+puts; good_combos.take(50).map{|combo| puts combo.slice(:area, :perim, :trees)}
